dbef4b115d5d951267c63e6fb2edc4bb303bf4e8
chixiang <chixiang@9fbank.cc>
2016-11-16 16:03:08 +0800

    删除多余代码

--- plugins/server-conf-merge/index.js
+++ plugins/server-conf-merge/index.js
@@ -4,7 +4,6 @@ const server = require('../../config/server/')
 
 module.exports = (name,obj) => {
 
-    console.log("name "+name+" url "+server[name].uri+obj.uri);
     const o = {
         uri:server[name].uri+obj.uri
     }
